c80cf8adf9080d4d752c099171918fa8588ff0b3
Levi Omagbemi <leviomagbemi15@gmail.com>
2023-12-08 11:22:38 +0100

    Implemented complete button and exit button

--- gui.py
+++ gui.py
@@ -7,11 +7,17 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-window = sg.Window("Todo App",
-                   layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]],
-                   font=("Poppins", 18))
+layout = [
+    [label],
+    [input_box, add_button],
+    [list_box, edit_button, complete_button],
+    [exit_button]
+         ]
+
+window = sg.Window("Todo App", layout=layout, font=("Poppins", 18))
 
 while True:
     event, value = window.read()
@@ -39,6 +45,18 @@ while True:
 
             window["todos"].update(values=todos)
             window["todo"].update(value='')
+
+        case "Complete":
+            complete_todo = value["todos"][0]
+            todos = functions.get_todos()
+
+            index = todos.remove(complete_todo)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value='')
+
+        case "Exit":
+            break
 
         case "todos":
             window["todo"].update(value=value["todos"][0])
